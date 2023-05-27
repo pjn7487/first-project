5dfbb78b2ed7c2149bb06ef271313b89eabb5605
Juneung Park <pjn5431@gmail.com>
2023-05-28 00:22:37 +0900

    222

--- _01_Operator1.java
+++ _01_Operator1.java
@@ -66,6 +66,6 @@ public class _01_Operator1 {
         System.out.println("대기 인원 : " + waiting++); //대기인원:2
         System.out.println("총 대기 인원 : " + waiting); //총 대기인원:3
 
-
+        //추가했따
     }
 }
